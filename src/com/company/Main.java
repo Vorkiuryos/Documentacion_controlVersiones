0abee7b8e16c1e527eee687e2aa7e6c611e73eea
Anomu <anomu_xz@hotmail.com>
2021-06-03 14:19:52 -1200

    prueba 2

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow World! Así es.");
+        System.out.println("Hellow World! Así es, sí.");
     }
 
     public String lol (String l, String o){
