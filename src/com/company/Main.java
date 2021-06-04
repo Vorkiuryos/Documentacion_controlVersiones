be0ad8a2bbd9c153a14887a64bd2839b4f66351f
Anomu <anomu_xz@hotmail.com>
2021-06-03 14:16:40 -1200

    cambio 1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hellow World!");
+        System.out.println("Hellow World! Así es.");
     }
 
     public String lol (String l, String o){
